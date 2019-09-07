fe3c91f0ebc0cf93d240e4386d562239ac65d2b2
Pechenka2005 <serg-20012001@mail.ru>
2019-09-08 01:35:21 +0300

    Удаление и просмотр выполненных

--- script.js
+++ script.js
@@ -7,22 +7,74 @@ function createTask(text){
     let newTask = document.createElement("li");
     let label = document.createElement("label");
     label.innerHTML =text;
-    let b1 = document.createElement("button");
-    b1.innerHTML = "Изменить";
     let b2 = document.createElement("button");
-    b2.innerHTML = "Удалить";
+    b2.className ="vBtn";
+    b2.innerHTML = "Выполнено";
     newTask.appendChild(label);
-    newTask.appendChild(b1);
     newTask.appendChild(b2);
+    
+
     return newTask;
 }
+function createTaskFinish(text){
+    let newTask = document.createElement("li");
+    let label = document.createElement("label");
+    label.classList.add("l")
+    label.innerHTML =text;
+    let b2 = document.createElement("button");
+    b2.className ="deleteBtn";
+    b2.innerHTML = "Удалить";
+    newTask.appendChild(label);
+    newTask.appendChild(b2);
+    
+
+    return newTask;
+}
+
+
 
 function AddTask(){
     if(addInput.value!=""){
         let task = createTask(addInput.value);
         tasks.appendChild(task);
+        bind(task);
         addInput.value ="";
     }
 }
+
 addButton.onclick = AddTask;
+
+
+function AddTaskFinish(){
+    let text = this.parentNode.firstChild.textContent;
+    let ListItem = createTaskFinish(text);
+    finish.appendChild(ListItem);
+    bind2(ListItem);
+    let task = this.parentNode;
+    ul = task.parentNode;
+    ul.removeChild(task);
+    
+
+}
+function taskDelete(){
+    let task = this.parentNode;
+    ul = task.parentNode;
+    ul.removeChild(task);
+}
+
+
+
+function bind(obj){
+    let vButton = obj.querySelector("button.vBtn");
+    //let delButton = obj.querySelector("button.deleteBtn");
+
+    vButton.onclick = AddTaskFinish;
+    //delButton.onclick = taskDelete;
+}
+
+function bind2(obj){
+    let delButton = obj.querySelector("button.deleteBtn");
+    
+    delButton.onclick = taskDelete;
+}
 
